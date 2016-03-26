da8428a9d19d3ea9d0ca2363300983d5f77d0051
Hendrik Schawe <surt91@gmail.com>
2016-03-26 11:13:35 +0100

    scaling script now listens to the shebang

--- scalingScript.sh
+++ scalingScript.sh
@@ -5,7 +5,7 @@ for MAX in  1.5 1.25 1.0 0.75 ;
 do
  for MIN in -2.25 -2.0 -1.75 -1.5 -1.25 -1. -0.75 ;
  do
-  python autoScale.py -f inputFiles.dat -o scaled_L512_256_128.out \
+  ./autoScale.py -f inputFiles.dat -o scaled_L512_256_128.out \
   	        -xc 0.5927 -a 0.75 -b 0.104 -showS -xr ${MIN} ${MAX}
  done
 done
